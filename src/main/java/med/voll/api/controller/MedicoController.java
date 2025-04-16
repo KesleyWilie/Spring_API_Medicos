d587e4c1bde5964112806316cdcd7df4bc9764e3
KesleyWilie <kesleywilie6@gmail.com>
2025-04-16 12:39:54 -0300

    :sparkles: Listagem de Médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,5 +23,9 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedicos(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<DadosListagemMedicos> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).stream().map(DadosListagemMedicos::new).toList();
     }
 }
